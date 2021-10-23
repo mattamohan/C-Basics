0f83f2c67575bc1f26cc55cb2f2a2cc28e705e84
mattamohan <92910670+mattamohan@users.noreply.github.com>
2021-10-23 12:34:02 +0530

    Add files via upload

---
+++ my details escape code.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("ROLL NO:20P35A0391\n\n NAME:MOHAN\n\n BRANCH:MECHANICAL\n\n COLLEGE:ACET ");
+}
---
+++ t.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	printf("STUDENT\tAGE\tCGPA\t\n");
+	printf("MOHAN\t19\t7.5\t");
+}
