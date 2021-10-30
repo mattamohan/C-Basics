2804e4e8fd2cb16077e0bc59cadd11c906181812
mattamohan <92910670+mattamohan@users.noreply.github.com>
2021-10-29 23:58:55 -0700

    Add files via upload

---
+++ mohan1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	//variables
+	int num1,num2,sum;
+	//initialization
+	num1=30;
+	num2=35;
+	//calculation
+	sum=num1+num2;
+	//output
+	printf("sum of %d and %d is %d",num1,num2,sum);
+}
---
+++ mohan1245.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+  	//variables
+  	float num1=10,num2=25,num3=45,num4=56,sum=num1+num2+num3+num4;
+  	//output
+  	printf("sum of %f,%f,%f and %f is %f",num1,num2,num3,num4,sum);
+}
